d171cebae90c3e0031078445c7d24afb164676dd
Oscar Pereiro <oscar.pereiro@thoughtworks.com>
2023-06-06 21:43:49 +0200

    Set up project and final test

---
+++ src/main/java/SnakeSequenceAnalyzer.java
@@ -0,0 +1,8 @@
+public class SnakeSequenceAnalyzer {
+    public SnakeSequenceAnalyzer(char[][] board, char[] sequence) {
+    }
+
+    public String analyze() {
+        return null;
+    }
+}
---
+++ src/test/java/SnakeSequenceAnalyzerTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SnakeSequenceAnalyzerTest {
+
+    private static final char[][] board = {
+            {'-', '-', 'x', '-', '-', '-'},
+            {'-', '-', 'x', '-', 'f', '-'},
+            {'-', '-', '-', '-', '-', '-'},
+            {'-', '-', 's', 's', 'S', '-'},
+            {'-', '-', 'x', 'x', 'x', '-'},
+            {'-', '-', '-', '-', '-', '-'}};
+    private static final char[] sequence1 = {'u', 'u'}; // should hit food -> return "food"
+    private static final char[] sequence2 = {'d'}; // should hit wall (x) -> return "game over"
+    private static final char[] sequence3 = {'r', 'r'}; // should hit out of bounds -> return "game over"
+    private static final char[] sequence4 = {'u', 'l', 'l', 'u'}; // should hit wall (x) -> return "game over"
+
+//    @ParameterizedTest
+//    @MethodSource("provideParameters")
+//    public void evaluateSequence_givenInput_shouldOutput(char[][] board, char[] sequence, String expectedResult) {
+//
+//        SnakeSequenceAnalyzer snakeSequenceAnalyzer = new SnakeSequenceAnalyzer(board, sequence);
+//
+//        String result = snakeSequenceAnalyzer.analyze();
+//
+//        Assertions.assertEquals(expectedResult, result);
+//    }
+//
+//    private static Stream<Arguments> provideParameters() {
+//        return Stream.of(
+//                Arguments.of(board, sequence1, "food"),
+//                Arguments.of(board, sequence2, "game over"),
+//                Arguments.of(board, sequence3, "game over"),
+//                Arguments.of(board, sequence4, "game over")
+//        );
+//    }
+}
