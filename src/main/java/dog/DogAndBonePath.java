27f9257b653757072f5fa547ea242e29d09ca6b9
Oscar Pereiro <oscar.pereiro@thoughtworks.com>
2023-06-07 11:23:38 +0200

    Set up Dog and Bone exercise witha assumption

---
+++ src/main/java/dog/DogAndBonePath.java
@@ -0,0 +1,8 @@
+package dog;
+
+// Assumption
+// I do not remember what the exercise was, given the title (Dog & Bone) and the fact that it´s supposed to be
+// dynamic programming, I will assume that we have a 2D matrix and we want to see the longest distance between a Dog and a Bone
+// Given there are multiple dogs and one bones (might do multiple bones after)
+public class DogAndBonePath {
+}
---
+++ src/test/java/dog/DogAndBonePathTest.java
@@ -0,0 +1,19 @@
+package dog;
+
+import org.junit.jupiter.api.Test;
+
+class DogAndBonePathTest {
+    private final char[][] matrix = {
+            {'-', '-','-', '-','-', '-'},
+            {'-', 'd','-', '-','-', '-'},
+            {'-', '-','-', '-','-', '-'},
+            {'-', 'd','-', '-','b', '-'},
+            {'-', '-','-', '-','-', '-'},
+            {'-', '-','-', '-','-', '-'}
+    };
+
+    @Test
+    public void longestPath_givenMatrix_shouldGiveLongestPath(){
+
+    }
+}
